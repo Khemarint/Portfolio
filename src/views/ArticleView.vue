9642c8fd6c5a48bfe7c33b4c13abae66f9abf016
Khemrent <Khemrent25@gmail.com>
2024-01-06 21:35:36 +0700

    Update

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="w-full md:w-3/5 h-20 mx-auto md:mt-5">
         <div class="bg-white rounded-xl mx-3 p-5 md:p-10 md:mx-0">
+            <button @click="$router.go(-1)" class="mb-4 bg-[#1e1e1f] hover:bg-[#282828] text-white text-sm py-2 px-3 rounded-xl">Go Back</button>
             <div>
                 <h1 class="text-xl md:text-4xl text-black text-left font-bold leading-relaxed">{{ title }}</h1>
                 <div class="mt-3 text-left text-gray-800 text-sm">Published at <span>{{ date }}</span></div>
